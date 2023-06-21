1a167124063076856ad438cd8afdf6a3b19f23a3
pablosimkievich <pablosimkievich@gmail.com>
2023-06-20 23:30:16 -0300

    script burger menu

---
+++ script.js
@@ -0,0 +1,23 @@
+const navslide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle nav
+        nav.classList.toggle('nav-active')
+
+        // Animación links
+        navLinks.forEach( (link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+            }
+        });
+        // Animación menú burger
+        burger.classList.toggle('toggle');
+    }); 
+}
+
+navslide();
